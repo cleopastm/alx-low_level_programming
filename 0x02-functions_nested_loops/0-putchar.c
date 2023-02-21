82130fa9cef09a8bf7de0ac80c4422f5ee28cacf
Tendai <madzimatendai534@gmail.com>
2023-02-21 13:35:27 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print putchar using a programm
+ * using mbah
+ * Return: 0
+ */
+
+int main(void)
+{
+_putcha('_');
+_putcha('p');
+_putcha('u');
+_putcha('t');
+_putcha('c');
+_putcha('h');
+_putcha('a');
+_putcha('r');
+_putcha('\n');
+return (0);
+}
