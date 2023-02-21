7a191f2ab8e89580f735bace8fcc52c34a2230ef
Tendai <madzimatendai534@gmail.com>
2023-02-21 14:16:04 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alphabet printing
+ * in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar('\n');
+
+	_putchar('\n');
+}
