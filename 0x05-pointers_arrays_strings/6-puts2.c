98ff3359ca607d52b90c6d4319c7e379813ef1c5
Tendai <madzimatendai534@gmail.com>
2023-02-27 11:03:47 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,7 @@
 void puts2(char *str)
 {
 	int longi = 0;
-	iint t = 0;
+	int t = 0;
 	char *y = str;
 	int o;
 
@@ -23,7 +23,7 @@ void puts2(char *str)
 	{
 		if (o % 2 == 0)
 	{
-		_putchar(str[o]);
+		putchar(str[o]);
 	}
 	}
 	_putchar('\n');
