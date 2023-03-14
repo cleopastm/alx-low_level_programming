2eb3fb97cb362fd092055b43b702c28ba14e9f3b
Tendai <madzimatendai534@gmail.com>
2023-03-14 10:16:29 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -59,7 +59,7 @@ char **strtow(char *str)
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
-				while (start < end)
+				while (start < end)						{
 					*tmp++ = str[start++];
 					*tmp = '\0';
 					matrix[k] = tmp - c;
