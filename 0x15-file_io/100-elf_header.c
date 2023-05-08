c4cd8a23c2f55d18d44c4f0da8bb8efc6b1d9920
Tendai madzima <madzimatendai534@gmail.com>
2023-05-08 22:22:42 +0200

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,34 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <elf.h>
-#include <fcntl.h>
+#include <stdint.h>
 #include <unistd.h>
+#include <string.h>
+#include <elf.h>
+
+#define MAX_BUFFER_SIZE 4096
+int main(int argc, char **argv)
+{
 
 /**
- * print_elf_header - prints the ELF header information
- * @header: pointer to the ELF header struct
+ * print_error - prints error message
+ * @message: pointer to the error message
+ *
+ * Return: 0
  */
-	int i;
+
+void print_error(char *message) {
+    fprintf(stderr, "%s\n", message);
+    exit(98);
+}
+/**
+* print_elf_header - prints contents of the header
+*
+* Return: 0
+*/
+
 void print_elf_header(Elf64_Ehdr *header)
 {
-    printf("ELF Header:\n");
-    printf("  Magic:   ");
-    for (i = 0; i < EI_NIDENT; i++)
-        printf("%02x%s", header->e_ident[i], i == EI_NIDENT - 1 ? "\n" : " ");
-    printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-    printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-    printf("  Version:                           %d%s\n", header->e_ident[EI_VERSION], header->e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
-    printf("  OS/ABI:                            %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "UNIX - Other");
-    printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-    printf("  Type:                              %s\n", header->e_type == ET_REL ? "REL (Relocatable file)" : header->e_type == ET_EXEC ? "EXEC (Executable file)" : header->e_type == ET_DYN ? "DYN (Shared object file)" : "unknown");
-    printf("  Entry point address:               %lx\n", (unsigned long)header->e_entry);
-    printf("  Start of program headers:          %ld (bytes into file)\n", header->e_phoff);
-    printf("  Start of section headers:          %ld (bytes into file)\n", header->e_shoff);
-    printf("  Flags:                             0x%x\n", header->e_flags);
-    printf("  Size of this header:               %d (bytes)\n", header->e_ehsize);
-    printf("  Size of program headers:           %d (bytes)\n", header->e_phentsize);
-    printf("  Number of program headers:         %d\n", header->e_phnum);
-    printf("  Size of section headers:           %d (bytes)\n", header->e_shentsize);
-    printf("  Number of section headers:         %d\n", header->e_shnum);
-    printf("  Section header string table index: %d\n", header->e_shstrndx);
+	int i;
+
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++) {
+        printf("%02x ", header->e_ident[i]);
+}
+	printf("\n");
+	printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+	printf("  Version:                           %d%s\n", header->e_ident[EI_VERSION], header->e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
+	printf("  OS/ABI:                            ");
+	switch (header->e_ident[EI_OSABI])
+{
+	case ELFOSABI_SYSV:
+		printf("UNIX - System V\n");
+		break;
+        case ELFOSABI_HPUX:
+        	    printf("HP-UX\n");
+            	break;
+        case ELFOSABI_NETBSD:
+            	printf("NetBSD\n");
+            	break;
+        case ELFOSABI_LINUX:
+            printf("Linux\n");
+            break;
+        case ELFOSABI_SOLARIS:
+		printf("Solaris\n");
+		break;
+	case ELFOSABI_IRIX:
+		printf("IRIX\n");
+		break;
+	case ELFOSABI_FREEBSD:
+		printf("FreeBSD\n");
+		break;
+	case ELFOSABI_TRU64:
+		printf("TRU64 UNIX\n");
+		break;
+	case ELFOSABI_ARM_AEABI:
+		printf("ARM EABI\n");
+		break;
+	case ELFOSABI_ARM:
+		printf("ARM\n");
+		break;
+	case ELFOSABI_STANDALONE:
+		printf("Standalone (embedded) application\n");
+		break;
+	default:
+		printf("<unknown: 0x%x>\n", header->e_ident[EI_OSABI]);
+		break;
+	}
+
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              ");
+	switch (header->e_type)
+{
+	case ET_NONE:
+		printf("NONE (No file type)\n");
+		break;
+	case ET_REL:
+		printf("REL (Relocatable file)\n");
+		break;
+	case ET_EXEC:
+		printf("EXEC (Executable file)\n");
+		break;
+	case ET_DYN:
+		printf("DYN (Shared object file)\n");
+		break;
+	case ET_CORE:
+		printf("CORE (Core file)\n");
+		break;
+	default:
+		printf("<unknown: 0x%x>\n", header->e_type);
+		break;
+}
+	printf("  Entry point address:               0x%lx\n", header->e_entry);
+	printf("  Start of program headers:          %ld (bytes into file)\n", header->e_phoff);
+	printf("  Start of section headers:          %ld (bytes into file)\n", header->e_shoff);
+	printf("  Flags:                             0x%x\n", header->e_flags);
+	printf("  Size of this header:               %d (bytes)\n", header->e_ehsize);
+	printf("  Size of program headers:           %d (bytes)\n", header->e_phentsize);
+	printf("  Number of program headers:         %d\n", header->e_phnum);
+	printf("  Size of section headers:           %d (bytes)\n", header->e_shentsize);
+	printf("  Number of section headers:         %d\n", header->e_shnum);
+	printf("  Section header string table index: %d\n", header->e_shstrndx);
+}
+return (0);
 }
